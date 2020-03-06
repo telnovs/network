fd7a9aef73ebe55d2d6c1b2b825e2db7b00e7a56
telnovs <telnov-s@mail.ru>
2020-03-07 01:40:48 +0700

    version 3

--- src/App.js
+++ src/App.js
@@ -1,38 +1,18 @@
 import React from 'react';
 import './App.css';
+import Header from "./components/Heder/Heder";
+import Profile from "./components/Profile/Profile";
+import Navbar from "./components/Navbar/Navbar";
 
 const App =() => {
     return (
-      <div className ='app-wrapper'>
-          <header className = 'header' >
-              <img src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTwZxjyeFzL8gtRFMMFx2TBLt4lYY3bg72UCV_7sGMtGliqMtNw'/>
-          </header>
-          <nav className = 'nav'>
-              <div>
-                 <a>Profile</a>
-              </div>
-              <div>
-                  <a>Messages</a>
-              </div>
-              <div>
-                  <a>News</a>
-              </div>
-              <div>
-                  <a>Music</a>
-              </div>
-              <div>
-                  <a>Settings</a>
-              </div>
-          </nav>
-          <div className ='content'>
-              <div>
-             <img src ='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQa6oUnm1b_f08m9G5ZSj6OgEUWbRN07MFKqJ22gEF2rjVu5jxF'/>
-              </div>
-              <div>
-                  <img src ='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRkF0FK0vrs0mvxOhn5M4Rqb2rrEXJinZYDFV_DY9FHK1HoH3pp '/>
-              </div>
-            </div>
-          </div>
+      <div className = 'app-wrapper'>
+          <Header/>
+          <Profile/>
+          <Navbar/>
+      </div>
+
+
 
     );
   };
---
+++ src/components/Profile/Mypost/Post/Post.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import s from './Post.module.css';
+
+
+const Post = () => {
+    return (
+        <div className={s.item}>
+            <img src='https://movies4maniacs.liberty.me/wp-content/uploads/sites/1218/2015/09/avatarsucks.jpg'/>
+            <div>
+                <span>like</span>
+            </div>
+        </div>
+    )
+};
+
+export default Post;
